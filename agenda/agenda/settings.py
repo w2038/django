c86cbbfb08221659c7beb018eb587297f8ceea6d
w2038 <w4gn3r2038@gmail.com>
2024-01-05 17:55:29 -0300

    config DB

--- agenda/agenda/settings.py
+++ agenda/agenda/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=h1xck-0($uyw7z@g)!g63lgwn$-fli06vt=vi+peohm!=-)+v'
+SECRET_KEY = 'django-insecure-%e1kd$yj*8-c!ytdfuz9dyf%ovxoqqn$euq570we0y326!_!lk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core'
 ]
 
 MIDDLEWARE = [
