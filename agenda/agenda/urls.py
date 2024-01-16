18f984a3ede96b14ce2c979d06bb2adc25fe6f58
w2038 <w4gn3r2038@gmail.com>
2024-01-16 15:47:28 -0300

    alteracao e exclusao de dados

--- agenda/agenda/urls.py
+++ agenda/agenda/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for agenda project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from core import views
@@ -24,6 +8,7 @@ urlpatterns = [
     path('agenda/', views.lista_eventos),
     path('agenda/evento/', views.evento),
     path('agenda/evento/submit', views.submit_evento),
+    path('agenda/evento/delete/<int:id_evento>', views.delete_evento),
     path('', RedirectView.as_view(url='/agenda/')),
     path('login/', views.login_user),
     path('login/submit', views.submit_login),
